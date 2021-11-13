b9e79f60e91ec12c0f53ddffafcf22aab1517f40
Ed5443 <efchang@ucsd.edu>
2021-11-12 16:10:12 -0800

    Finished explore

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -34,3 +34,51 @@ test('hello@gmail.ucsd.net is not email', () =>{
 test('1234567@1234567.12345 is not email', () =>{
     expect(functions.isEmail('1234567@1234567.12345')).toBe(false);
 });
+
+test('password_ is password', () =>{
+    expect(functions.isStrongPassword('password_')).toBe(true);
+});
+
+test('Edward12345678 is password', () =>{
+    expect(functions.isStrongPassword('Edward12345678')).toBe(true);
+});
+
+test('hi is not password', () =>{
+    expect(functions.isStrongPassword('hi')).toBe(false);
+});
+
+test('1234567@1234567 is not password', () =>{
+    expect(functions.isStrongPassword('1234567@1234567')).toBe(false);
+});
+
+test('12/9/2021 is date', () =>{
+    expect(functions.isDate('12/9/2021')).toBe(true);
+});
+
+test('1/29/1999 is date', () =>{
+    expect(functions.isDate('1/29/1999')).toBe(true);
+});
+
+test('2/22/22 is not date', () =>{
+    expect(functions.isDate('2/22/22')).toBe(false);
+});
+
+test('1999/12/25 is not date', () =>{
+    expect(functions.isDate('1999/12/25')).toBe(false);
+});
+
+test('a34f23 is hex color', () =>{
+    expect(functions.isHexColor('a34f23')).toBe(true);
+});
+
+test('ABC is hex color', () =>{
+    expect(functions.isHexColor('ABC')).toBe(true);
+});
+
+test('gh99f23 is not hex color', () =>{
+    expect(functions.isHexColor('gh99f23')).toBe(false);
+});
+
+test('1234 is not hex color', () =>{
+    expect(functions.isHexColor('1234')).toBe(false);
+});
